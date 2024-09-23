85803f81c837d1248f2d74c78947f7d0ccfeaf5a
Daigo Shitara <shitara@tagbangers.co.jp>
2024-09-23 10:32:33 +0900

    list rooms in sidebar

--- lib/slaks_web/live/chat_root_live.ex
+++ lib/slaks_web/live/chat_root_live.ex
@@ -6,12 +6,33 @@ defmodule SlaksWeb.ChatRoomLive do
   @spec render(any()) :: Phoenix.LiveView.Rendered.t()
   def render(assigns) do
     ~H"""
+    <div class="flex flex-col flex-shrink-0 w-64 bg-slate-100">
+      <div class="flex justify-between items-center flex-shrink-0 h-16 border-b border-slate-300 px-4">
+        <div class="flex flex-col gap-1.5">
+          <h1 class="text-lg font-bold text-gray-800">
+            Slax
+          </h1>
+        </div>
+      </div>
+
+      <div class="mt-4 overflow-auto">
+        <div class="flex items-center h-8 px-3 group">
+          <span class="ml-2 leading-none font-medium text-sm">Rooms</span>
+        </div>
+
+        <div id="rooms-list">
+          <.room_link :for={room <- @rooms} room={room} active={room.id == @room.id} />
+        </div>
+      </div>
+    </div>
+
     <div class="flex flex-col flex-grow shadow-lg">
       <div class="flex justify-between items-center flex-shrink-0 h-16 bg-white border-b border-slate-300 px-4">
         <div class="flex flex-col gap-1.5">
           <h1 class="text-sm font-bold leading-none">
             #<%= @room.name %>
           </h1>
+
           <div class="text-xs leading-none h-3.5" phx-click="toggle-topic">
             <%= if @hide_topic? do %>
               <span class="text-slate-600">[Topic hidden]</span>
@@ -25,14 +46,32 @@ defmodule SlaksWeb.ChatRoomLive do
     """
   end
 
+  attr :active, :boolean, required: true
+  attr :room, Room, required: true
+
+  defp room_link(assigns) do
+    ~H"""
+    <a
+      class={[
+        "flex items-center h-8 text-sm pl-8 pr-3",
+        (@active && "bg-slate-300") || "hover:bg-slate-300"
+      ]}
+      href="#"
+    >
+      <.icon name="hero-hashtag" class="h-4 w-4" />
+      <span class={["ml-2 leading-note", @active && "font-bold"]}>
+        <%= @room.name %>
+      </span>
+    </a>
+    """
+  end
+
   # opens websocket connection, performs initialization to first render the page
   def mount(_params, _session, socket) do
-    room =
-      Room
-      |> Repo.all()
-      |> List.first()
+    rooms = Room |> Repo.all()
+    room = List.first(rooms)
 
-    {:ok, assign(socket, hide_topic?: false, room: room)}
+    {:ok, assign(socket, hide_topic?: false, room: room, rooms: rooms)}
   end
 
   def handle_event("toggle-topic", _params, socket) do
