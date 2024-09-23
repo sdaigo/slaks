355096691752855d52ca6b1bb50327cfbd834395
Daigo Shitara <shitara@tagbangers.co.jp>
2024-09-23 10:55:19 +0900

    feat: patch to navigate room

--- lib/slaks_web/live/chat_root_live.ex
+++ lib/slaks_web/live/chat_root_live.ex
@@ -51,32 +51,35 @@ defmodule SlaksWeb.ChatRoomLive do
 
   defp room_link(assigns) do
     ~H"""
-    <a
+    <.link
       class={[
         "flex items-center h-8 text-sm pl-8 pr-3",
         (@active && "bg-slate-300") || "hover:bg-slate-300"
       ]}
-      href={~p"/rooms/#{@room.id}"}
+      patch={~p"/rooms/#{@room}"}
     >
       <.icon name="hero-hashtag" class="h-4 w-4" />
       <span class={["ml-2 leading-note", @active && "font-bold"]}>
         <%= @room.name %>
       </span>
-    </a>
+    </.link>
     """
   end
 
   # opens websocket connection, performs initialization to first render the page
-  def mount(params, _session, socket) do
+  def mount(_params, _session, socket) do
     rooms = Repo.all(Room)
+    {:ok, assign(socket, rooms: rooms)}
+  end
 
+  def handle_params(params, _session, socket) do
     room =
       case Map.fetch(params, "id") do
         {:ok, id} -> Repo.get!(Room, id)
-        :error -> List.first(rooms)
+        :error -> List.first(socket.assigns.rooms)
       end
 
-    {:ok, assign(socket, hide_topic?: false, room: room, rooms: rooms)}
+    {:noreply, assign(socket, hide_topic?: false, room: room)}
   end
 
   def handle_event("toggle-topic", _params, socket) do
